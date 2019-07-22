1e615f9b5b619b77dfb152fd7b0d71c1a7a329a8
aleisabl <aleisabl.25@gmail.com>
2019-07-22 14:17:48 -0400

    pasando el approuter a un navbar de bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,33 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
+      /*  <NavLink to="/" exact={true} activeClassName="is-active">Home </NavLink>
+       <br></br>
+       <NavLink to="/register" activeClassName="is-active">Register </NavLink> */
+
+
 const Header = () => {
     return (
-    <header>
-       <h1>ProyectoMOMS</h1>
-       <NavLink to="/" exact={true} activeClassName="is-active">Home </NavLink>
-       <br></br>
-       <NavLink to="/register" activeClassName="is-active">Register </NavLink>
-    </header>
+        <header>
+
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a className="navbar-brand" href="#">ProyectoMOMS</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarText">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/register">Registro</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+        </header>
     )
 }
 
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import NotFoundPage from '../components/NotFoundPage'
 import HomePage from '../components/HomePage'
 import Register from '../components/Register'
