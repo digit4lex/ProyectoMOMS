17b9689cf21166651eb653d517ffa03bf0298cd4
aleisabl <aleisabl.25@gmail.com>
2019-07-23 11:16:21 -0400

    arreglando css de parrafos de inicio

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,20 +6,11 @@ import './../styles/HomePage.css'
 const HomePage = () => {
     let style={
         h3: {
-            position: "absolute",
-            width: "302px",
-            height: "52px",
-            left: "40px",
-            top: "98px",
-
-            /* h3 */
-
             fontFamily: "Montserrat",
             fontStyle: "normal",
             fontWeight: "normal",
             fontSize: "18px",
             lineHeight: "22px",
-            display: "flex",
             alignItems: "center",
             textAlign: "center",
 
@@ -29,18 +20,11 @@ const HomePage = () => {
             color: "#FFFFFF"
         },
         h2: {
-            position: "absolute",
-            width: "310px",
-            height: "140px",
-            left: "33px",
-            top: "157px",
-
             fontFamily: "Playfair Display",
             fontStyle: "normal",
             fontWeight: "normal",
             fontSize: "26px",
             lineHeight: "35px",
-            display: "flex",
             alignItems: "center",
             textAlign: "center",
             color: "#FFFFFF"
