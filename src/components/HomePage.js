913c6315b143cbd2dd3c66dba2655a7a250cddbd
valeskaperezrretamal <jperezr@udd.cl>
2019-07-23 10:54:49 -0400

    se modifican elementos de pagina principal

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import FrameLogin from './FrameLogin'
-import HomeLogin from './HomeLogin'
+///import HomeLogin from './HomeLogin'
 import './../styles/HomePage.css'
 
 const HomePage = () => {
@@ -52,7 +52,6 @@ const HomePage = () => {
         <div>
             <h3 style={style.h3}> Más de mil mujeres y compañias nos avalan </h3>
             <h1 style={style.h2}>Conectamos el talento femenino con las mejores empresas que tienen ofertas flexibles para ellas</h1>
-        <HomeLogin/>
         </div>
         {/* <HomeLogin/> */}
         {/* <Login/> */}
