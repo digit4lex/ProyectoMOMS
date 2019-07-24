63ec6098549c8b11673fb76284cc27a82038c047
valeskaperezrretamal <jperezr@udd.cl>
2019-07-24 00:42:22 -0400

    se le agrega ruta de componente

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,8 +3,10 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import NotFoundPage from '../components/NotFoundPage';
 import HomePage from '../components/HomePage';
 import Register from '../components/Register';
+import PersonalData from '../components/PersonalData'
 import Header from '../components/Header';
 import Login from '../components/Login';
+
 
 const AppRouter = () => (
     <BrowserRouter>
@@ -15,6 +17,7 @@ const AppRouter = () => (
         <Switch>
          <Route path="/" component={HomePage} exact={true}/>
          <Route path="/register" component={Register} exact={true}/>
+         <Route path="/sobreti" component={PersonalData} exact={true}/>
          <Route path="/login" component={Login} exact={true}/>
          <Route component={NotFoundPage}/>
         </Switch>
